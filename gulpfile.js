2cae1cc9469310bc5512b4f25fe633ca4fc38ae6
Dmitriy Akulich <3700010@gmail.com>
2017-09-28 00:25:49 +0300

    установка gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var less = require('gulp-less');
+var gulp = require('gulp');
+
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public/css'));
+});
