559e665edcc76201ca8fd5c6d8dc5bf2c7cd6f69
Dmitriy Akulich <3700010@gmail.com>
2017-10-16 11:52:44 +0300

    Адаптивные изображения

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var buttonOpenForm = document.querySelector('.product__button');
+var formSection = document.querySelector('.modal-form');
+var form = document.querySelector('.modal-form__form');
+var formOpen = document.querySelector('.modal-form--open');
+var goodsBuy = document.querySelector('.goods__buy');
 
 navMain.classList.add('main-nav--closed');
 
@@ -12,3 +17,26 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+buttonOpenForm.addEventListener('click', openForm);
+
+function openForm(e) {
+  e.preventDefault();
+  formSection.classList.toggle('modal-form--open');
+}
+
+form.onsubmit = function(event) {
+  event.preventDefault();
+
+  formSection.classList.remove('modal-form--open');
+  formSection.classList.add('modal-form--none');
+
+  alert("Спасибо, Ваша заявка принята!");
+}
+
+goodsBuy.addEventListener('click', openForm2);
+
+function openForm2(o) {
+  o.preventDefault();
+  formSection.classList.toggle('modal-form--open');
+};
